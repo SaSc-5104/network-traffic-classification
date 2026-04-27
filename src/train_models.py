d4cc6568b28caab5d720ca7ddd520ce1ae7dd0f6
levikwm <144151304+levikwm@users.noreply.github.com>
2026-04-27 15:56:18 -0400

    Update train_models.py
    random_stats typo to random_state line 17

--- src/train_models.py
+++ src/train_models.py
@@ -14,7 +14,7 @@ df = pd.read_csv("data/processed/features.csv")
 X = df.drop('label', axis = 1)
 y = df['label']
 
-X_temp, X_test, y_temp, y_test = train_test_split(X, y, test_size = 0.2, random_stats = 42, stratify = y)
+X_temp, X_test, y_temp, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42, stratify = y)
 X_train, X_val, y_train, y_val = train_test_split(X_temp, y_temp, test_size = 0.25, random_state = 42, stratify = y_temp)
 
 print(f"Train: {len(X_train)}, Val: {len(X_val)}, Test: {len(X_test)}")

bc99ea4324fce968681f7b7641f4b906e4f69581
timmyc181 <timothy.j.cleveland1@gmail.com>
2026-04-29 12:46:56 -0400

    Made feature extraction more efficient with PcapReader, added more sniffed data for Amazon.

--- src/extract_features.py
+++ src/extract_features.py
@@ -1,16 +1,20 @@
 import csv, statistics
 from pathlib import Path
-from scapy.all import rdpcap
+from scapy.all import PcapReader
 
 def extract_features(pcap_path, label):
-    try:
-        packets = rdpcap(str(pcap_path))
-    except Exception as e:
-        print(f"Skipping {pcap_path.name}: {e}")
-        return None
+    times = []
+    lengths = []
 
-    times   = [float(p.time) for p in packets if hasattr(p, 'time')]
-    lengths = [len(p) for p in packets]
+    try:
+        with PcapReader(str(pcap_path)) as packets:
+            for p in packets:
+                if hasattr(p, "time"):
+                    times.append(float(p.time))
+                    lengths.append(len(p))
+    except Exception as e:
+        print(f"\rSkipping {pcap_path.name}: {e}")
+        return None
 
     if len(times) < 2:
         return None
@@ -18,25 +22,33 @@ def extract_features(pcap_path, label):
     intervals = [times[i+1] - times[i] for i in range(len(times)-1)]
 
     return {
-        'packet_count':        len(packets),
-        'total_length':        sum(lengths),
-        'avg_interval':        statistics.mean(intervals),
-        'max_interval':        max(intervals),
-        'min_interval':        min(intervals),
-        'avg_length':          statistics.mean(lengths),
-        'max_length':          max(lengths),
-        'min_length':          min(lengths),
-        'most_common_length':  max(set(lengths), key=lengths.count),
-        'label':               label
+        "packet_count": len(lengths),
+        "total_length": sum(lengths),
+        "avg_interval": statistics.mean(intervals),
+        "max_interval": max(intervals),
+        "min_interval": min(intervals),
+        "avg_length": statistics.mean(lengths),
+        "max_length": max(lengths),
+        "min_length": min(lengths),
+        "most_common_length": max(set(lengths), key=lengths.count),
+        "label": label,
     }
 
+
+directory = list(Path("data/raw").glob("*.pcapng"))
+to_process = len(directory)
+processed = 0
+
 rows = []
-for f in Path("data/raw").glob("*.pcapng"):
+for f in directory:
+    print(f"\r[{processed}/{to_process}] Processing {f.name}...".ljust(60), end="", flush=True)
+
     label = f.stem.rsplit('_', 1)[0]
     row = extract_features(f, label)
     if row:
         rows.append(row)
-    print(f"Processed {f.name} → {row['label'] if row else 'skipped'}")
+
+    processed += 1
 
 with open("data/processed/features.csv", "w", newline="") as f:
     w = csv.DictWriter(f, fieldnames=rows[0].keys())
